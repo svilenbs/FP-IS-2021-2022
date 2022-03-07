661e24a536beff0113337b3bd52b36482a045d22
svilenbs@gmail.com <svilenbs@gmail.com>
2022-03-07 07:50:37 +0200

    week 3 new version and move lists to week 4

--- week03/week-03.hs
+++ week03/week-03.hs
@@ -1,23 +1,23 @@
 {--
-List, almost everything around lists
+More recursion
 --}
 
--- Write function length1
--- Write function elem1
--- Write function reverse1
--- Write function take1
--- Write function drop1
--- Write function getLastElement returning the last element in a list
--- Write function maximum1 returning the biggest element in a list 
--- Write function getLastButOneElement returning the element before the last
--- getLastButOneElement [1,2,3,42,1,2] -> 1
--- Write function union1 for two lists
--- Write function sum on an Integer list. In case of empty list, result should be 1.
--- Write function multiple on an Integer list
--- Write function avg1 returning the average element (sum/length) of a list
--- Write function zip1 working with list and returning list of 2 elements list
--- (**) Drop every N'th element from a list. Problem 16
--- Rotate a list N places to the left. Problem 19
--- Run-length encoding of a list Problem 10
--- encoding1 "aaasdsss" == [(3,'a'), (1,'s'), (1,'d'), (3,'s')]
--- Write function that generate all possible lists for a^2 + b^2 = c^2 for up to number c
+
+{-- Find m-th summation of first n natural numbers.
+If m > 1
+  SUM(n, m) = SUM(SUM(n, m - 1), 1)
+Else 
+  SUM(n, 1) = Sum of first n natural numbers.
+--}
+{--Write function calculateF
+calculateF(n) = n, if n < 3
+calculateF(n) = calculateF(n - 1) + 2*calculateF(n - 2) + calculateFf(n - 3), otherwise
+-- }
+-- Write function coundDigits n a b ,calculating how many time there is the digit n in range of Integers [a,b]
+-- Write function calculateSin n by the formula in week03-task.png
+{--
+Calculate the numbers of all n-digit strictly increasing numbers
+--}
+-- Write function sumOfPrimes finding the sum of all prime numbers in the interval [a, b]
+-- Write function canbePrime n finding if there is a way by remove one of the digits in n the number became a prime.
+
---
+++ week04/week-04.hs
@@ -0,0 +1,28 @@
+{--
+List, almost everything around lists
+--}
+
+-- Write function length1
+-- Write function elem1
+-- Write function reverse1
+-- Write function take1
+-- Write function drop1
+-- Write function getLastElement returning the last element in a list
+-- Write function maximum1 returning the biggest element in a list 
+-- Write function getLastButOneElement returning the element before the last
+-- getLastButOneElement [1,2,3,42,1,2] -> 1
+-- Write function union1 for two lists
+-- Write function sum on an Integer list. In case of empty list, result should be 1.
+-- Write function multiple on an Integer list
+-- Write function avg1 returning the average element (sum/length) of a list
+-- Write function zip1 working with list and returning list of 2 elements list
+-- (**) Drop every N'th element from a list. Problem 16
+-- Rotate a list N places to the left. Problem 19
+-- Run-length encoding of a list Problem 10
+-- encoding1 "aaasdsss" == [(3,'a'), (1,'s'), (1,'d'), (3,'s')]
+-- Write function that generate all possible lists for a^2 + b^2 = c^2 for up to number c
+
+
+sum1 a b c = a*(b+c)
+
+sum2 = sum1 4
