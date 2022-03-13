e2dbf5b5f373b1caf6b09032b096a5beefc7e538
svilenbs@gmail.com <svilenbs@gmail.com>
2022-03-13 11:52:06 +0200

    week04 small updates

--- week04/week-04.hs
+++ week04/week-04.hs
@@ -1,5 +1,6 @@
 {--
 List, almost everything around lists
+Partially higher order functions
 --}
 
 -- Write function length1
@@ -13,16 +14,14 @@ List, almost everything around lists
 -- getLastButOneElement [1,2,3,42,1,2] -> 1
 -- Write function union1 for two lists
 -- Write function sum on an Integer list. In case of empty list, result should be 1.
--- Write function multiple on an Integer list
+-- Write function multiple on an Integer list.
 -- Write function avg1 returning the average element (sum/length) of a list
--- Write function zip1 working with list and returning list of 2 elements list
+-- Write function zip1 working with list and returning list of 2 elements list. Make the same with tuple. 
+-- Write function primesList returning list of all primes numbers
+-- Write function isAnyPrimes [Int] -> Bool retuning True if there is a prime number in the list, False otherwise
 -- (**) Drop every N'th element from a list. Problem 16
 -- Rotate a list N places to the left. Problem 19
 -- Run-length encoding of a list Problem 10
 -- encoding1 "aaasdsss" == [(3,'a'), (1,'s'), (1,'d'), (3,'s')]
--- Write function that generate all possible lists for a^2 + b^2 = c^2 for up to number c
-
-
-sum1 a b c = a*(b+c)
-
-sum2 = sum1 4
+-- Write function that generate all possible tuple lists (Integer, Integer, Integer) for a^2 + b^2 = c^2 for up to number c
+-- Based on the [(String, Number)] represented for name of a city and temperature, return the name of the city with the highest temperature from the list.
