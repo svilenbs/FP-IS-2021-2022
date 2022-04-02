3f1f855ac56b9ff4e45cdc75b977c5661b0cb788
svilenbs@gmail.com <svilenbs@gmail.com>
2022-04-02 17:23:27 +0300

    week07 tasks

---
+++ week07/week07.hs
@@ -0,0 +1,17 @@
+-- By list [a,b,c...] defining a + b*x+c*x^2... define function
+-- calculate function find l returning 2 args lambda function returning n-th derivative in spesific point
+
+
+-- https://raw.githubusercontent.com/SimeonHristov99/FP_IS_Summer_21-22/main/Week_05%20-%20Lambda%20Expressions.%20Tuples/assets/forHomeTask6.png
+-- https://raw.githubusercontent.com/SimeonHristov99/FP_IS_Summer_21-22/main/Week_05%20-%20Lambda%20Expressions.%20Tuples/assets/forHomeTask5.png
+
+-- Sort from the previous time
+
+-- Modules Data.List
+
+-- Find if one matrix is symetric. Is it possible in one line?
+-- In String find the Char that is more ofen define one after another as a subset
+-- TesttEst" -> 't'
+-- "ValueeeeeVVV" -> 'e'
+
+-- What is find, what is returning find... Types....
