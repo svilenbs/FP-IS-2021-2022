a3c28d4e22566d68a80a41e15a59eb5f48c2e20a
svilenbs@gmail.com <svilenbs@gmail.com>
2022-05-12 20:18:30 +0300

    week11 topics

---
+++ week11/week11.hs
@@ -0,0 +1,16 @@
+-- Lets talk about Trees again...
+
+-- Return sum of all elements in a Tree
+
+-- Confirm if a Tree is BST. In case to be True this statement B>=Y and Z>B should be true for each Tree/subtree
+--- B 
+-- / \
+--Y  Z
+
+-- Find sum of all elements on level I in a Tree
+-- Check if we have a number in a Tree that is meet more than once in the Tree?
+
+-- Graph....
+-- Problem 81
+-- (**) Path from one node to another one
+-- Write a function that, given two nodes a and b in a graph, returns all the acyclic paths from a to b.
